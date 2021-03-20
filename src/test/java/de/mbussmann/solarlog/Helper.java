2c68e9e8989857aa92e483611d0149b7dd04a4d5
ManuelBu96 <manu199644@gmail.com>
2021-03-20 14:49:03 +0100

    Update Test algorithm

---
+++ src/test/java/de/mbussmann/solarlog/Helper.java
@@ -0,0 +1,141 @@
+/*
+ *
+ * Copyright 2021 - Manuel Bußmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package de.mbussmann.solarlog;
+
+import de.mbussmann.solarlog.boundary.dto.AuthenticationDto;
+import de.mbussmann.solarlog.boundary.dto.RegistrationDto;
+import de.mbussmann.solarlog.boundary.dto.UserDto;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+import org.flywaydb.core.Flyway;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+@ApplicationScoped
+public class Helper {
+    @Inject
+    Flyway flyway;
+
+    private Map<String, String> userHeaders = new HashMap<String,String>();
+    private Map<String, String> adminHeaders = new HashMap<String,String>();
+    private String userToken;
+    private String adminToken;
+    private RegistrationDto regUser = new RegistrationDto("test@test.de", "Max", "Mustermann", "test");
+    private AuthenticationDto loginUser = new AuthenticationDto("test@test.de", "test");
+    private AuthenticationDto loginAdmin = new AuthenticationDto("admin@admin.com", "admin");
+
+    public void loginUser() {
+        //Generate the User token
+        userToken = given()
+                .contentType(ContentType.JSON)
+                .body(loginUser)
+                .when()
+                .post("authentication/login")
+                .getBody()
+                .asString();
+
+        userHeaders.put("Accept","application/json");
+        userHeaders.put("Authorization", "Bearer " + userToken);
+    }
+
+    public void loginAdmin() {
+        //Generate the token
+        adminToken = given()
+                .contentType(ContentType.JSON)
+                .body(loginAdmin)
+                .when()
+                .post("authentication/login")
+                .getBody()
+                .asString();
+
+        adminHeaders.put("Accept","application/json");
+        adminHeaders.put("Authorization", "Bearer " + adminToken);
+    }
+
+    public UserDto getUser(Map<String, String> header) {
+        this.loginUser();
+        ValidatableResponse res = given()
+                .headers(userHeaders)
+                .when()
+                .get("/user")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+        return res.extract().body().as(UserDto.class);
+    }
+
+    public void destruct() {
+        //Reset Database
+        flyway.clean();
+        flyway.migrate();
+        //Register User
+        given()
+                .contentType(ContentType.JSON)
+                .body(regUser)
+                .when()
+                .post("authentication/register")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    public void destroy() {
+        userHeaders.clear();
+        adminHeaders.clear();
+        userToken = "";
+        adminToken = "";
+    }
+
+    public Map<String, String> getUserHeaders() {
+        return userHeaders;
+    }
+
+    public Map<String, String> getAdminHeaders() {
+        return adminHeaders;
+    }
+
+    public RegistrationDto getRegUser() {
+        return regUser;
+    }
+
+    public void setRegUser(RegistrationDto regUser) {
+        this.regUser = regUser;
+    }
+
+    public AuthenticationDto getLoginUser() {
+        return loginUser;
+    }
+
+    public void setLoginUser(AuthenticationDto loginUser) {
+        this.loginUser = loginUser;
+    }
+
+    public AuthenticationDto getLoginAdmin() {
+        return loginAdmin;
+    }
+
+    public void setLoginAdmin(AuthenticationDto loginAdmin) {
+        this.loginAdmin = loginAdmin;
+    }
+}
--- src/test/java/de/mbussmann/solarlog/UserRessource.java
+++ src/test/java/de/mbussmann/solarlog/UserRessource.java
@@ -1,56 +1,38 @@
 package de.mbussmann.solarlog;
 
-import de.mbussmann.solarlog.boundary.dto.AuthenticationDto;
-import de.mbussmann.solarlog.boundary.dto.RegistrationDto;
-
 import de.mbussmann.solarlog.boundary.dto.UserDto;
+import de.mbussmann.solarlog.entity.UserRole;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.inject.Inject;
 
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 public class UserRessource {
-    private RegistrationDto regUser = new RegistrationDto("test@test.de", "Max", "Mustermann", "test");
-    private AuthenticationDto loginUser = new AuthenticationDto("test@test.de", "test");
-    private Map<String, String> headers = new HashMap<String,String>();
-    private String token;
+    @Inject
+    Helper helper;
 
-    @BeforeAll
-    public void setup() {
-        //Register
-        given()
-                .contentType(ContentType.JSON)
-                .body(regUser)
-                .when()
-                .post("authentication/register")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK);
+    @BeforeEach
+    public void destruct() {
+        helper.destruct();
+    }
 
-        //Generate the token
-        token = given()
-                .contentType(ContentType.JSON)
-                .body(loginUser)
-                .when()
-                .post("authentication/login")
-                .jsonPath()
-                .get("token");
-
-        headers.put("Accept","application/json");
-        headers.put("Authorization", "Bearer " + token);
+    @AfterEach
+    public void destroy() {
+        helper.destroy();
     }
 
     @Test
+    @Order(1)
+    @DisplayName("Get User")
     public void testGetUserRessource() {
+        helper.loginUser();
         given()
-                .headers(headers)
+                .headers(helper.getUserHeaders())
                 .when()
                 .get("/user")
                 .then()
@@ -58,14 +40,28 @@ public class UserRessource {
     }
 
     @Test
-    public void testPutUserRessource() {
-        UserDto dto = new UserDto();
-        dto.setFirstName("Maximilian");
-        dto.setLastName("Mustermann");
-        dto.setEmail(loginUser.getEmail());
-        dto.setRole("USER");
+    @Order(2)
+    @DisplayName("Get Admin")
+    public void testGetAdminRessource() {
+        helper.loginAdmin();
         given()
-                .headers(headers)
+                .headers(helper.getAdminHeaders())
+                .when()
+                .get("/user")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Update User-Data")
+    public void testPutUserRessource() {
+        helper.loginUser();
+        UserDto dto = helper.getUser(helper.getUserHeaders());
+        dto.setFirstName("Maximilian");
+        dto.setLastName("Lustig");
+        given()
+                .headers(helper.getUserHeaders())
                 .contentType(ContentType.JSON)
                 .body(dto)
                 .put("/user")
@@ -74,9 +70,44 @@ public class UserRessource {
     }
 
     @Test
-    public void testDeleteUserRessource() {
+    @Order(4)
+    @DisplayName("Update User-Role - ADMIN")
+    public void testPutUserIdRessource() {
+        helper.loginAdmin();
+        //User ID 1
+        UserDto dto = helper.getUser(helper.getUserHeaders());
+        dto.setRole((UserRole.ADMINISTRATOR).toString());
         given()
-                .headers(headers)
+                .headers(helper.getAdminHeaders())
+                .contentType(ContentType.JSON)
+                .body(dto)
+                .put("/user/1")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Delete User - ADMIN")
+    public void testDeleteUserIdRessource() {
+        helper.loginAdmin();
+        //User ID 1
+        given()
+                .headers(helper.getAdminHeaders())
+                .contentType(ContentType.JSON)
+                .body("id:1")
+                .delete("/user/1")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Delete Own User")
+    public void testDeleteUserRessource() {
+        helper.loginUser();
+        given()
+                .headers(helper.getUserHeaders())
                 .contentType(ContentType.JSON)
                 .body("id:2")
                 .delete("/user")
